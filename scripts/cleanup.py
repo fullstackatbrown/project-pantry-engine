666208a4211c7a3d9cf0703a6258933ad3f1eeed
Alex Ding <alexander_ding@brown.edu>
2021-03-08 15:10:30 -0500

    Integrated scrape.py with elasticsearch

--- scripts/cleanup.py
+++ scripts/cleanup.py
@@ -15,17 +15,7 @@ Somewhat a copy of Sami's recipe-browser/RecipeScraper/data_models.py
 
 query = {'query': {'match_all': {}}}
 
-# print(client.indices.delete("recipe_index"))
-# print(client.indices.delete("url_index"))
-
-# print(client.delete_by_query(index="recipe_index", body=query))
-# print(client.delete_by_query(index="url_index", body=query))
-
-ri_hits = (client.search(body=query, index='recipe_index'))['hits']['hits']
-ui_hits = (client.search(body=query, index='url_index'))['hits']['hits']
-
-if len(ri_hits) == 0:
-    print('\'recipe_index\' is clear')
-if len(ui_hits) == 0:
-    print('\'url_index\' is clear')
-
+print("Clearing recipe_index")
+client.delete_by_query(index="recipe_index", body=query)
+client.indices.delete("recipe_index")
+print("Done")
