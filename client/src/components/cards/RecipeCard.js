115ef8846ac598f2267ef5f813abb7cafa1e324b
Ian Balaguera <balagueraian@gmail.com>
2021-03-21 17:54:20 -0400

    Add recipe preview component

--- client/src/components/cards/RecipeCard.js
+++ client/src/components/cards/RecipeCard.js
@@ -1,10 +1,40 @@
-import React, { Component } from 'react';
-import { Card, Row, Col, Image, ListGroup } from 'react-bootstrap';
+import React from 'react';
+import { Card, Row, Col, Image, ListGroup, Modal, Button } from 'react-bootstrap';
 
-class RecipeCard extends Component {
-    render() {
-        return (
-            <Card className="recipe-card floating">
+function RecipePreview(props) {
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="recipe-preview-name">
+                    recipe name
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Row>
+                    <Image className="recipe-preview-image" src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872" />
+                </Row>
+                <Row className="recipe-preview-text">
+                    <h2>in my pantry</h2>
+                    <p>eggs, tomatoes, peppers, onions, jalapeños</p>
+                    <h2>i need</h2>
+                    <p className="ingredients-missing">paprika, sour cream, pita bread</p>
+                </Row>
+            </Modal.Body>
+        </Modal>
+    );
+}
+
+export default function RecipeCard() {
+    const [modalShow, setModalShow] = React.useState(false);
+
+    return (
+        <>
+            <Card className="recipe-card floating" onClick={() => setModalShow(true)}>
                 <Card.Body className="p-0">
                     <Row className="m-0">
                         <Col xs={5} className="m-0 p-2">
@@ -20,8 +50,12 @@ class RecipeCard extends Component {
                     </Row>
                 </Card.Body>
             </Card>
-        );
-    }
-}
 
-export default RecipeCard;
+            <RecipePreview
+                className="recipe-preview"
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+            />
+        </>
+    );
+}
