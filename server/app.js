72ded30d9187673c8b15b06410cb815d1cd950dc
mmmeeeker <mmmeeeker@gmail.com>
2021-04-11 15:46:21 -0400

    Routing works. Use localhost ... /?ing[]=foo&ing[]=bar&... to input ingredient list. Wider error handling needs to be done, eg., bad params and on 404

--- server/app.js
+++ server/app.js
@@ -18,8 +18,8 @@ app.use('/recipes', indexRouter);
 
 
 app.listen(3000, function() {});
-// TODO --> what should happen on bad input/ user inputs bogus URL? ie, .../asdf
 
+// TODO --> what should happen on bad input/ user inputs bogus URL? ie, .../asdf
 // Will we have some sort of 404 page?
 app.use(function(req,res,next){
     res.send('404 route not found');
