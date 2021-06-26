8135edcbe99f04f630a2744fa77f7ead3b0f0aec
awang158 <awang158@cs.brown.edu>
2021-06-26 03:01:19 -0400

    fix server

--- server/app.js
+++ server/app.js
@@ -3,26 +3,22 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const indexRouter = require('./routes/index');
+// const createError = require('http-errors');
+
 const app = express();
-const createError = require('http-errors');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static("public"));
 app.use(cookieParser());
 
 // Routers
 app.use('/', indexRouter);
-app.use('/recipes', indexRouter);
-
-
-app.listen(3000, function() {});
 
 // TODO --> what should happen on bad input/ user inputs bogus URL? ie, .../asdf
 // Will we have some sort of 404 page?
-app.use(function(req,res,next){
-    res.send('404 route not found');
-})
+// app.use(function(req,res,next){
+//     res.send('404 route not found');
+// })
 
 module.exports = app;
